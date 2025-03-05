0e710696dc2f40f70c8e9f9cd0b0ef80064985f9
George Panayides <georgepanayides@MacBook-Pro.local>
2025-03-05 18:55:12 +1000

    Playing around a bit more

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,2 +1,17 @@
 public class TestClass {
+
+    int num;
+    String string;
+
+    public TestClass(int num, String string) {
+        this.num = num;
+        this.string = string;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        TestClass test = new TestClass(1, "Test");
+    }
 }
